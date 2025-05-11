5af4407c7e84ca23f8ec4ef2996e4328d6e69448
angeon <ckman92@naver.com>
2025-05-11 15:29:05 +0900

    프로그래머스:ab출력하기

---
+++ src/programmers기초/ab출력하기/Main20250509.java
@@ -0,0 +1,14 @@
+package programmers기초.ab출력하기;
+
+import java.util.Scanner;
+
+public class Main20250509 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("a = "+ a);
+        System.out.println("b = " + b);
+    }
+}
