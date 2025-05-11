47203a7c66403dff86de59d237136adfdcff55e9
angeon <ckman92@naver.com>
2025-05-11 15:29:13 +0900

    프로그래머스:문자열 출력하기

---
+++ src/programmers기초/문자열출력하기/Main20250509.java
@@ -0,0 +1,11 @@
+package programmers기초.문자열출력하기;
+
+import java.util.Scanner;
+
+public class Main20250509 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str);
+    }
+}
